ea25a8bbaae7eef6d9da2b24d892d2ace0d95550
Yana Soboleva <luxuriouslystellarsky@gmail.com>
2026-03-15 20:00:47 +0200

    Update App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,22 +5,24 @@ import MovieGrid from '../MovieGrid/MovieGrid';
 import Loader from '../Loader/Loader';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import MovieModal from '../MovieModal/MovieModal';
-import { fetchMovies} from '../../services/movieService';
-import type {TMDBResponse} from '../../services/movieService';
+import { fetchMovies, type TMDBResponse} from '../../services/movieService';
 import type { Movie } from '../../types/movie';
-import { useQuery } from '@tanstack/react-query';
+
 import ReactPaginate from 'react-paginate';
 import css from './App.module.css';
+import { useQuery, keepPreviousData} from '@tanstack/react-query';
 
 const App = () => {
   const [searchQuery, setSearchQuery] = useState<string>('');
   const [page, setPage] = useState<number>(1);
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
 
-  const { data, isLoading, isError } = useQuery<TMDBResponse>({
+  const { data, isLoading, isError, isFetching } = useQuery<TMDBResponse>({
     queryKey: ['movies', searchQuery, page],
     queryFn: () => fetchMovies(searchQuery, page),
     enabled: !!searchQuery,
+    placeholderData: keepPreviousData,
+    // placeholderData: (previousData) => previousData,
   });
 
   const movies = data?.results ?? [];
@@ -28,13 +30,13 @@ const App = () => {
 
 
   useEffect(() => {
-    if (!isLoading && !isError && searchQuery && movies.length === 0) {
+    if (!isFetching && !isLoading && !isError && searchQuery && movies.length === 0) {
       toast("No movies found for your request.", {
         icon: '🔍',
         position: "top-center",
       });
     }
-  }, [movies.length, isLoading, isError, searchQuery]);
+  }, [movies.length, isFetching, isLoading, isError, searchQuery]);
 
   const handleSearch = (query: string) => {
     if (query.trim() === '') {
@@ -42,6 +44,7 @@ const App = () => {
       return;
     }
     setSearchQuery(query);
+    setPage(1);
   };
 
   return (
